d20e34d2cb6574715e487d7d3170400b73adb6d9
khasanjon <khasanjon.dev@gmail.com>
2023-12-01 23:02:22 +0500

    update some actions

--- bot/logic/start.py
+++ bot/logic/start.py
@@ -5,7 +5,7 @@ from aiogram.types import KeyboardButton
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
 from bot.data import data
-from bot.requests.user import get_or_create_user
+from bot.requests.user import update_or_create_user
 from bot.utils.keyboardbuilder import keyboard_builder
 from bot.utils.states import states
 
@@ -15,23 +15,35 @@ router = Router(name='start')
 @router.message(states.phone)
 async def phone_handler(message: types.Message, state: FSMContext) -> None:
     if message.content_type == types.ContentType.CONTACT:
-        pass
-    elif message.text.startswith('+998') and len(message.text) == 13:
-        pass
+        context = {
+            'telegram_id': message.from_user.id,
+            'phone': message.contact.phone_number
+        }
+        await update_or_create_user(context)
+    else:
+        await state.set_state(states.city)
+        await city_handler(message, state)
+    await state.clear()
+    await message.answer(f'Xush kelibsiz {message.from_user.first_name}')
 
 
 @router.message(states.city)
 async def city_handler(message: types.Message, state: FSMContext) -> None:
     if message.text in data['cities']:
-        # await state.set_state(states.phone)
-        await state.clear()
-        text = ("Ro'yxatga olish uchun telefon raqamingizni kiriting!\n"
-                "Masalan +998xx xxx xx xx")
-        button = KeyboardButton(text="📞Mening telefon raqamim", request_contact=True)
-        markup = ReplyKeyboardBuilder().add(button).as_markup(resize_keyboard=True)
-        await message.answer(text, reply_markup=markup)
+        context = {
+            'telegram_id': message.from_user.id,
+            'city': message.text
+        }
+        await update_or_create_user(context)
+        await state.set_state(states.phone)
+        await phone_handler(message, state)
     else:
-        await state.set_state(states.city)
+        await state.set_state(states.language)
+        await language_handler(message, state)
+    text = "Ro'yxatga olish uchun telefon raqamingizni yuboring!"
+    button = KeyboardButton(text="📞Mening telefon raqamim", request_contact=True)
+    markup = ReplyKeyboardBuilder().add(button).as_markup(resize_keyboard=True)
+    await message.answer(text, reply_markup=markup)
 
 
 @router.message(states.language)
@@ -41,13 +53,14 @@ async def language_handler(message: types.Message, state: FSMContext) -> None:
             'telegram_id': message.from_user.id,
             'language': message.text
         }
-        await get_or_create_user(context)
+        await update_or_create_user(context)
         await state.set_state(states.city)
-        reply_markup = await keyboard_builder(data['cities'], [2])
-        await message.answer('Shaharni tanlang', reply_markup=reply_markup)
+        await city_handler(message, state)
     else:
         await state.set_state(states.start)
         await start_handler(message, state)
+    reply_markup = await keyboard_builder(data['cities'], [2])
+    await message.answer('Shaharni tanlang', reply_markup=reply_markup)
 
 
 @router.message(states.start)
@@ -72,7 +85,7 @@ async def first_start_handler(message: types.Message, state: FSMContext) -> None
     context = {
         'telegram_id': message.from_user.id
     }
-    await get_or_create_user(context)
+    await update_or_create_user(context)
     user = message.from_user
     text = (f"Assalomu alaykum {user.first_name}. Men Oqtepa Lavash yetkazib berish xizmati botiman!\n"
             f"Привет {user.first_name}! Я бот службы доставки Oqtepa Lavash!\n"
