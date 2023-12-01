4af55846769fd4192d25fd32aa888c85f903e27f
khasanjon <khasanjon.dev@gmail.com>
2023-12-01 07:25:24 +0500

    update some actions

--- bot/logic/start.py
+++ bot/logic/start.py
@@ -5,7 +5,7 @@ from aiogram.filters import CommandStart
 from aiogram.fsm.context import FSMContext
 
 from bot.data.data import language
-from bot.requests.users import get_user_or_create
+from bot.requests.users import get_or_create_user
 from bot.utils.keyboardbuilder import keyboard_builder
 from bot.utils.states import states
 
@@ -14,8 +14,9 @@ start_router = Router(name='start')
 
 @start_router.message(CommandStart())
 async def start_handler(message: types.Message, state: FSMContext) -> None:
-    user = await get_user_or_create(message.from_user.id)
-    await state.set_state(states.language)
+    user = await get_or_create_user(message.from_user.id)
+    if not user['language']:
+        await state.set_state(states.language)
     reply_markup = await keyboard_builder(language.values(), [1])
     user = message.from_user
     first_message = (f"Assalomu alaykum {user.first_name}. Men Oqtepa Lavash yetkazib berish xizmati botiman!\n"
--- bot/requests/users.py
+++ bot/requests/users.py
@@ -3,10 +3,10 @@ import httpx
 from root import settings
 
 
-async def get_user_or_create(telegram_id):
+async def get_or_create_user(telegram_id):
     url = settings.base_url + 'user/user/'
     data = {
         'telegram_id': telegram_id
     }
     response = httpx.post(url, data=data)
-    return response
+    return response.json()
