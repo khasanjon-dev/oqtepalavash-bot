95c401896a63eee60123bc648c833af9c1745b6b
khasanjon <khasanjon.dev@gmail.com>
2023-12-02 10:26:26 +0500

    update some files

--- bot/logic/__init__.py
+++ bot/logic/__init__.py
@@ -1,6 +1,4 @@
 """This package is used for a bot logic implementation."""
-from .echo import echo_router
-from .help import help_router
 from .start import router
 
-routers = (router, help_router, echo_router)
+routers = (router,)
