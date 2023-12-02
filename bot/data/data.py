7c6f9e4626e1be2b215c05119ab230f8572abb4c
khasanjon <khasanjon.dev@gmail.com>
2023-12-02 10:43:34 +0500

    add inline keyboard builder

--- bot/data/data.py
+++ bot/data/data.py
@@ -1,4 +1,6 @@
 data = {
     'languages': ["🇺🇿 O'zbekcha", "🇷🇺 Русский", "🇺🇸 English"],
-    'cities': ["Tashkent", "Nukus", "Namangan", "Qo'qon", "Andijon", "Farg'ona", "Samarqand", "G'azalkent"]
+    'cities': ["Tashkent", "Nukus", "Namangan", "Qo'qon", "Andijon", "Farg'ona", "Samarqand", "G'azalkent"],
+    'menus': ["🛒 Buyurtma berish", "ℹ️ Biz haqimizda", "🛍 Buyurtmalarim", "🏘 Filiallar", "✍️ Fikr bildirish",
+              "⚙️ Sozlamalar"]
 }
