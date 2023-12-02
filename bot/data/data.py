a4d498962814811bb93cc7a99890ac0ff6185138
khasanjon <khasanjon.dev@gmail.com>
2023-12-02 23:27:19 +0500

    update some files

--- bot/data/data.py
+++ bot/data/data.py
@@ -1,6 +1,10 @@
 data = {
     'languages': ["🇺🇿 O'zbekcha", "🇷🇺 Русский", "🇺🇸 English"],
     'cities': ["Tashkent", "Nukus", "Namangan", "Qo'qon", "Andijon", "Farg'ona", "Samarqand", "G'azalkent"],
-    'menus': ["🛒 Buyurtma berish", "ℹ️ Biz haqimizda", "🛍 Buyurtmalarim", "🏘 Filiallar", "✍️ Fikr bildirish",
-              "⚙️ Sozlamalar"]
+    'menus': {
+        'texts': ["🛒 Buyurtma berish", "ℹ️ Biz haqimizda", "🛍 Buyurtmalarim", "🏘 Filiallar", "✍️ Fikr bildirish",
+                  "⚙️ Sozlamalar"],
+        'call_backs': ['order', 'about', 'orders', 'states', 'write', 'settings']
+    },
+    'delivery_menu': ['🛵 Eltib berish', '🚶‍♂️ Borib olish'],
 }
