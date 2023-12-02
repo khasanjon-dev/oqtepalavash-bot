3f2714ac6cfe133aa4d0b69ea10ebca5e15102c0
khasanjon <khasanjon.dev@gmail.com>
2023-12-02 23:27:20 +0500

    update some files

--- bot/utils/inlinekeyboardbuilder.py
+++ bot/utils/inlinekeyboardbuilder.py
@@ -1,12 +1,31 @@
+from enum import Enum
+
+from aiogram.filters.callback_data import CallbackData
 from aiogram.types import InlineKeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
-async def inline_keyboard_builder(messages: list, sizes: list | None = None):
+class Menu(str, Enum):
+    order = 'order'
+    about = 'about'
+    orders = 'orders'
+    states = 'states'
+    write = 'write'
+    settings = 'settings'
+
+
+class MenuCallBack(CallbackData, prefix="menu"):
+    choice: Menu
+
+
+def inline_keyboard_builder(messages: list[str], call_backs: list[str], sizes: list | None = None):
     builder = InlineKeyboardBuilder()
-    for message in messages:
+    for i in range(len(messages)):
         builder.add(
-            InlineKeyboardButton(text=message, callback_data='ok')
+            InlineKeyboardButton(
+                text=messages[i],
+                callback_data=MenuCallBack(choice=call_backs[i]).pack()
+            )
         )
     builder.adjust(*sizes)
     return builder.as_markup()
