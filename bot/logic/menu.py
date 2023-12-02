b139943013a7a01d9f4f55d164b6577e32de1f02
khasanjon <khasanjon.dev@gmail.com>
2023-12-02 10:43:46 +0500

    add inline keyboard builder

--- bot/logic/menu.py
+++ bot/logic/menu.py
@@ -2,6 +2,8 @@ from aiogram import Router, types
 from aiogram.enums import ParseMode
 from aiogram.fsm.context import FSMContext
 
+from bot.data import data
+from bot.utils.inlinekeyboardbuilder import inline_keyboard_builder
 from bot.utils.states import states
 
 menu = Router()
@@ -13,5 +15,7 @@ async def menu_handler(message: types.Message, state: FSMContext) -> None:
             f"Buyurtma qilish tugmasini bosing\n\n"
             f"Shuningdek, aksiyalarni ko'rishingiz va bizning filiallar bilan tanishishingiz mumkin\n\n"
             f"<a href='https://telegra.ph/Taomnoma-09-30'>Oqtepa Lavash menu</a>")
-    await message.answer(text, parse_mode=ParseMode.HTML)
+    markup = await inline_keyboard_builder(data['menus'], [1, 2, 1, 2])
+    await message.answer(text, parse_mode=ParseMode.HTML, reply_markup=markup)
     await state.clear()
+
