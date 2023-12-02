faa14a70651c347bacfcd11fdbcc920d160d40bf
khasanjon <khasanjon.dev@gmail.com>
2023-12-02 23:27:25 +0500

    update some files

--- bot/utils/states.py
+++ bot/utils/states.py
@@ -1,12 +1,23 @@
 from aiogram.fsm.state import StatesGroup, State
 
 
-class States(StatesGroup):
+class Register(StatesGroup):
     start = State()
     language = State()
     city = State()
     phone = State()
+
+
+class Menu(StatesGroup):
+    first_menu = State()
     menu = State()
 
 
-states = States()
+class Order(StatesGroup):
+    menu = State()
+    location = State()
+
+
+register_states = Register()
+menu_states = Menu()
+order_states = Order()
