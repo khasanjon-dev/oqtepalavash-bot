b36ebe271c602ac08985122b309bc40c5e2aeac4
khasanjon <khasanjon.dev@gmail.com>
2023-12-03 20:49:05 +0500

    update some actions

--- bot/logic/__init__.py
+++ bot/logic/__init__.py
@@ -1,6 +1,7 @@
 """This package is used for a bot logic implementation."""
 from .call_backs import callback_router, order_router
 from .menu import menu_router
+from .product import product_router
 from .register import register_router
 
-routers = (register_router, menu_router, callback_router, order_router)
+routers = (register_router, menu_router, callback_router, order_router, product_router)
