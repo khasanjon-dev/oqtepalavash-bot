54daa621d9eac2d124b5ca50be06d717c19d6828
khasanjon <khasanjon.dev@gmail.com>
2023-12-03 16:10:36 +0500

    update some actions

--- bot/__main__.py
+++ bot/__main__.py
@@ -3,9 +3,9 @@ import asyncio
 import logging
 
 from aiogram import Bot
-from aiogram.fsm.storage.memory import MemoryStorage
+from redis.asyncio.client import Redis
 
-from bot.dispatcher import get_dispatcher
+from bot.dispatcher import get_dispatcher, get_redis_storage
 from bot.utils.set_bot_commands import set_default_commands
 from root import settings
 
@@ -13,16 +13,16 @@ from root import settings
 async def start_bot():
     """This function will start bot with polling mode."""
     bot = Bot(token=settings.bot.token)
-    # storage = get_redis_storage(
-    #     redis=Redis(
-    #         db=settings.redis.db,
-    #         host=settings.redis.host,
-    #         password=settings.redis.passwd,
-    #         username=settings.redis.username,
-    #         port=settings.redis.port,
-    #     )
-    # )
-    dp = get_dispatcher(storage=MemoryStorage())
+    storage = get_redis_storage(
+        redis=Redis(
+            db=settings.redis.db,
+            host=settings.redis.host,
+            password=settings.redis.passwd,
+            username=settings.redis.username,
+            port=settings.redis.port,
+        )
+    )
+    dp = get_dispatcher(storage=storage)
     await set_default_commands(bot)
     await dp.start_polling(
         bot,
