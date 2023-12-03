fff83c40c2061876aa5cbd4fd4b320dbfffa7c84
khasanjon <khasanjon.dev@gmail.com>
2023-12-03 20:49:08 +0500

    update some actions

--- bot/logic/call_backs.py
+++ bot/logic/call_backs.py
@@ -5,8 +5,8 @@ from aiogram.types import CallbackQuery, KeyboardButton
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
 from bot.data import data, orders_data
-from bot.logic.menu import menu_handler
-from bot.utils.inlinekeyboardbuilder import MenuCallBack
+from bot.logic.menu import main_menu_handler
+from bot.utils.callback_class import MenuCallBack
 from bot.utils.keyboardbuilder import keyboard_builder
 from bot.utils.locations import get_address
 from bot.utils.states import order_states, menu_states
@@ -18,8 +18,8 @@ order_router = Router(name='order_router')
 @order_router.message(order_states.confirm_location)
 async def confirm_location_handler(message: types.Message, state: FSMContext) -> None:
     if message.text in orders_data['confirm_menu']:
-        await state.set_state(menu_states.menu)
-        await menu_handler(message, state)
+        await state.set_state(menu_states.main_menu)
+        await main_menu_handler(message, state)
     else:
         markup = keyboard_builder(orders_data['confirm_menu'], [2])
         address = get_address(message.location.latitude, message.location.longitude)
@@ -28,7 +28,7 @@ async def confirm_location_handler(message: types.Message, state: FSMContext) ->
 
 
 @order_router.message(order_states.location)
-async def order_get_location(message: types.Message, state: FSMContext) -> None:
+async def order_get_location_handler(message: types.Message, state: FSMContext) -> None:
     if message.content_type == types.ContentType.LOCATION:
         await state.set_state(order_states.confirm_location)
         await confirm_location_handler(message, state)
@@ -45,7 +45,7 @@ async def order_menu_handler(message: types.Message, state: FSMContext) -> None:
     if message.text in data['delivery_menu']:
         await state.set_state(order_states.location)
         await state.update_data(reception_type=[data['delivery_menu'].get(message.text, None), message.text])
-        await order_get_location(message, state)
+        await order_get_location_handler(message, state)
     else:
         markup = keyboard_builder(data['delivery_menu'].keys(), [2])
         await message.answer('Buyurtma turini tanlang', reply_markup=markup)
