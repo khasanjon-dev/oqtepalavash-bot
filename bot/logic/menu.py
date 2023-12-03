3d6145a53714f23e186865493a03df34dc3a3118
khasanjon <khasanjon.dev@gmail.com>
2023-12-03 20:49:13 +0500

    update some actions

--- bot/logic/menu.py
+++ bot/logic/menu.py
@@ -1,8 +1,10 @@
 from aiogram import Router, types
 from aiogram.enums import ParseMode
 from aiogram.fsm.context import FSMContext
+from aiogram.types import ReplyKeyboardRemove
 
-from bot.data import data
+from bot.data import data, menu_data
+from bot.utils.callback_class import MenuCallBack, MainMenuCallBack
 from bot.utils.inlinekeyboardbuilder import inline_keyboard_builder
 from bot.utils.states import menu_states
 
@@ -10,13 +12,13 @@ menu_router = Router()
 
 
 async def menu_message(message: types.Message) -> None:
-    await message.answer('Buyurtmani birga joylashtiramizmi? 🤗')
+    await message.answer('Buyurtmani birga joylashtiramizmi? 🤗', reply_markup=ReplyKeyboardRemove())
 
 
 @menu_router.message(menu_states.first_menu)
 async def first_menu_handler(message: types.Message, state: FSMContext) -> None:
     text = 'Quyidagilardan birini tanlang 👇'
-    markup = inline_keyboard_builder(data['menu'].keys(), data['menu'].values(), [1, 2, 1, 2])
+    markup = inline_keyboard_builder(MenuCallBack, data['menu'].keys(), data['menu'].values(), [1, 2, 1, 2])
     await state.set_state(menu_states.menu)
     await message.answer(text, reply_markup=markup)
 
@@ -32,5 +34,25 @@ async def menu_handler(message: types.Message, state: FSMContext) -> None:
                 "Buyurtma qilish tugmasini bosing\n\n"
                 "Shuningdek, aksiyalarni ko'rishingiz va bizning filiallar bilan tanishishingiz mumkin\n\n"
                 "<a href='https://telegra.ph/Taomnoma-09-30'>Oqtepa Lavash menu</a>")
-        markup = inline_keyboard_builder(data['menu'].keys(), data['menu'].values(), [1, 2, 1, 2])
+        markup = inline_keyboard_builder(MenuCallBack, data['menu'].keys(), data['menu'].values(), [1, 2, 1, 2])
         await message.answer(text, ParseMode.HTML, reply_markup=markup)
+
+
+@menu_router.message(menu_states.main_menu)
+async def main_menu_handler(msg: types.Message, state: FSMContext):
+    if msg.text == '/start':
+        await state.set_state(menu_states.menu)
+        await menu_handler(msg, state)
+    else:
+        await msg.delete()
+        await menu_message(msg)
+        text = ("Buyurtma qaytadan boshlash uchun 🆕 Yangi buyurtma yaratish tugmasini bosing\n\n"
+                "Shuningdek, aksiyalarni ko'rishingiz va bizning filiallar bilan tanishishingiz mumkin\n\n"
+                "<a href='https://telegra.ph/Taomnoma-09-30'>Oqtepa Lavash menu</a>")
+        markup = inline_keyboard_builder(
+            MainMenuCallBack,
+            menu_data['main_menu'].keys(),
+            menu_data['main_menu'].values(),
+            [1, 2, 1, 2, 1]
+        )
+        await msg.answer(text, ParseMode.HTML, reply_markup=markup)
